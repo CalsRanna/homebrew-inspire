ce98cb450734b23cbd88650500a1074c6bd9b010
Cals <cals@CalsdeMac-mini.local>
2024-11-15 12:04:23 +0800

    feat: add Homebrew formula for flit
    Based on the diff, this commit adds a new Homebrew formula for flit, an AI-powered Conventional Commits Generator. The formula specifies the binary download URL, SHA256 hash, and basic installation instructions.

---
+++ flit.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Flit < Formula
+  desc "AI-powered Conventional Commits Generator"
+  homepage "https://github.com/CalsRanna/auto_commit"
+  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.0/flit"
+  sha256 "489fcd926c129c12699e8f10f231148853e7366cc6680f3c789d174e0ac99567"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "flit"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test flit`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    # system "false"
+  end
+end
