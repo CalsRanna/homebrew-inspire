7db3c42e939dd9e6361590565c0633df0d4c6aa0
Cals <cals@CalsdeMac-mini.local>
2024-12-20 16:08:41 +0800

    chore(deps): update Flit to version 1.0.1

--- flit.rb
+++ flit.rb
@@ -1,8 +1,8 @@
 class Flit < Formula
   desc "AI-powered Conventional Commits Generator"
   homepage "https://github.com/CalsRanna/auto_commit"
-  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.0/flit"
-  sha256 "a2674cd8ab897cf47a13f99a33d24d79c95bd570aad8e8c8057a6cc5cedbd817"
+  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.1/flit"
+  sha256 "680159cf7d03feb927a9ea2eff76d1f5a6fde5b12dfdba499c1aaeb9a2e70f42"
   license "MIT"
 
   def install
