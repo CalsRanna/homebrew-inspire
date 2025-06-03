6ea1428c09b66d59ea79de7915139e64212bc4a8
Cals <cals@CalsdeMac-mini.local>
2025-06-03 10:59:08 +0800

    chore(deps): update flit download URL and SHA256 to v1.0.5

--- flit.rb
+++ flit.rb
@@ -1,8 +1,8 @@
 class Flit < Formula
   desc "AI-powered Conventional Commits Generator"
   homepage "https://github.com/CalsRanna/auto_commit"
-  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.4/flit"
-  sha256 "33360d472dff5cb4c6da85884e9dff6ad9c29a77687491db1b79ed46faafca19"
+  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.5/flit"
+  sha256 "29b4710e4f50300d915e91174cd96240945add05fdc47d522bebcd7082ac30c0"
   license "MIT"
 
   def install
