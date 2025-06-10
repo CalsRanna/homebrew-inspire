881ee59def5d99b985a012c09c9e006aa3679c16
Cals <cals@CalsdeMac-mini.local>
2025-06-10 14:20:56 +0800

    chore: update flit formula to version 1.0.8
    Update the download URL and SHA256 checksum to reflect the new release version.

--- flit.rb
+++ flit.rb
@@ -1,8 +1,8 @@
 class Flit < Formula
   desc "AI-powered Conventional Commits Generator"
   homepage "https://github.com/CalsRanna/auto_commit"
-  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.7/flit"
-  sha256 "f6cf68559687272033e61b8a611153a8caf402e186dfe83009086c0aeca0d018"
+  url "https://github.com/CalsRanna/auto_commit/releases/download/v1.0.8/flit"
+  sha256 "1df71097814268cefa73f04c514695f22a25783c14deb6e1ce2d3709140a25c7"
   license "MIT"
 
   def install
